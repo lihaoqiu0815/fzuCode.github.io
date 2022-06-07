432c9dc434795afaf474505d00cce456ea148d88
lihaoqiu <lihao.qiu.200108@gmail.com>
2022-06-07 13:45:05 +0800

    博学

--- second_edition/src/views/Login.vue
+++ second_edition/src/views/Login.vue
@@ -98,7 +98,7 @@ export default {
     }
     let info = ref('')
     info.value = '民主'
-    const columns = ['富强', '民主', '文明', '和谐', '自强', '诚信', '敬业', '友善', '平等', '公正', '法治', '爱国', '友善', '志诚', '远志', '博爱']
+    const columns = ['富强', '民主', '文明', '和谐', '自强', '诚信', '敬业', '友善', '平等', '公正', '法治', '爱国', '友善', '志诚', '远志', '博学']
     const onConfirm = () => {}
     const onChange = value => {
       info.value = value
